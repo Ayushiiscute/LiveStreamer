efee3fe575075f205cf2aad664942f1d76299552
Ayushiiscute <kapur.ayushi1511@gmail.com>
2025-02-17 11:06:40 +0530

    navbar

---
+++ app/(browse)/_components/sidebar/index.tsx
@@ -0,0 +1,15 @@
+// import { Toggle } from "./toggle"
+
+
+import { Toggle } from "./toggle";
+import { Wrapper } from "./wrapper";
+
+ export const Sidebar=()=>{
+    return (
+       
+        <Wrapper>
+           <Toggle/>
+           
+         </Wrapper>
+    );
+ };
